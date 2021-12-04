8f77b2a4ef9495657f01d223ebeb409cf2b5ed26
Diane Galiano <dianegaliano09@gmail.com>
2021-12-04 12:06:56 -0600

    created and finished server.js file

---
+++ server.js
@@ -0,0 +1,25 @@
+const e = require('express');
+const express = require('express');
+const mongoose = require('mongoose');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+app.use(require('./routes/api/apiRoutes'));
+app.use(require('./routes/homeRoutes'));
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+  });
+  
