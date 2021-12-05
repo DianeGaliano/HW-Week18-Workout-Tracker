868bbdde099c627b67cd23894fb637a04372f5b9
Diane Galiano <dianegaliano09@gmail.com>
2021-12-04 18:27:03 -0600

    fixing errors

--- routes/api.js
+++ routes/api.js
@@ -1,72 +1,54 @@
-const db = require('../models/Workout');
+const Workout = require('../models/workout');
 const router = require("express").Router();
 
 
-router.get('/api/workouts', (req, res) => {
-    db.Workout.find({})
+router.get('/api/workout', (req, res) => {
+    Workout.find({})
     .then((dbWorkout) => {
-        dbWorkout.forEach((workout) => {
-            var total = 0;
-            workout.exercises.forEach((e) => {
-                total += e.duration;
-            });
-            workout.totalDuration = total;
-        });
         res.json(dbWorkout);
     })
-    .catch(err => {
+    .catch((err) => {
         res.status(400).json(err);
     });
 });
 
 // new workout
-router.post('/api/workouts', ({body}, res) => {
-    db.Workout.create(body)
+router.post('/api/workout', ({body}, res) => {
+    Workout.create(body)
     .then((dbWorkout) => {
         res.json(dbWorkout);
     })
-    .catch(err => {
+    .catch((err) => {
         res.status(400).json(err);
     });
 });
 
 // update workout
-router.put('/api/workouts/:id', (req, res) => {
-    db.Workout.findOneAndUpdate(
-        {
-            _id: req.params.id
-        },
+router.put('/api/workout/:id', (req, res) => {
+    Workout.findOneAndUpdate(
+        params.id,
         {
             $push: {
                 exercise: body
-            },
-            $inc: { 
-                totalDuration: req.body.duration 
-            },
+            }
         },
         {
             new: true,
-           
+            runValidators: true
         }
     )
-    .then((dbWorkout) => {
-        res.json(dbWorkout);
-    })
-    .catch(err => {
+    .then((data) => res.json(data))
+    .catch((err) => {
         res.status(400).json(err);
 
     });
 });
 
 // get workouts in range
-router.get('/api/workouts/range', (req, res) => {
-    db.Workout.find({})
-    .then((dbWorkout) => {
-        consol.log("WORKOUTS");
-        consol.log(dbWorkout);
-        res.json(dbWorkout);
-    })
-    .catch(err => {
+router.get('/api/workout/range', (req, res) => {
+    Workout.find({})
+    .then((data) => res.json(data))
+    .catch((err) => {
         res.status(400).json(err);
 
     });
